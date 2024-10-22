ad13f7717677043cd5cfb32a520c0ae3672b4817
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2024-10-22 23:37:03 +0530

    useRef is used to keep track of the number

--- muz-react-app/src/App.tsx
+++ muz-react-app/src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/alert";
+// import React, { useState, useEffect, useRef } from 'react';
+
 function App() {
   let items = ["New York", "Tokiya", "London", "Paris"];
 
   const [alertVisible, setAlertvisible] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const renderCount = useRef(0);
+
+  useEffect(() => {
+    renderCount.current += 1;
+  });
 
   const handleSelectItem = (item: String) => {
     console.log(item);
@@ -19,6 +27,14 @@ function App() {
       />
       {alertVisible && <Alert onClose={() => setAlertvisible(false)}></Alert>}
       <Button onClick={() => setAlertvisible(true)}> My Button </Button>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <h1>Render Count: {renderCount.current}</h1>
+      </div>
     </div>
   );
 }

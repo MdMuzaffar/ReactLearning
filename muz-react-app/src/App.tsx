9931d186ee49e0aaa97c6282a5d47333ef9d996b
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2024-10-24 23:47:51 +0530

    React Practice on useState

--- muz-react-app/src/App.tsx
+++ muz-react-app/src/App.tsx
@@ -2,7 +2,17 @@ import { useState, useEffect, useRef } from "react";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/alert";
+import GitHubUser from "./components/GitHubUser";
 // import React, { useState, useEffect, useRef } from 'react';
+
+// function GitHubUser({ title, name }: any) {
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//       <h1>{title}</h1>
+//     </div>
+//   );
+// }
 
 function App() {
   let items = ["New York", "Tokiya", "London", "Paris"];
@@ -18,25 +28,38 @@ function App() {
   const handleSelectItem = (item: String) => {
     console.log(item);
   };
-  return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      {alertVisible && <Alert onClose={() => setAlertvisible(false)}></Alert>}
-      <Button onClick={() => setAlertvisible(true)}> My Button </Button>
+
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((response) => response.json())
+      .then(setData);
+    // console.log(data);
+  });
+  if (data)
+    return (
       <div>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+        <div>
+          <GitHubUser name={data} title={data} />
+        </div>
+
+        <ListGroup
+          items={items}
+          heading="Cities"
+          onSelectItem={handleSelectItem}
         />
-        <h1>Render Count: {renderCount.current}</h1>
+        {alertVisible && <Alert onClose={() => setAlertvisible(false)}></Alert>}
+        <Button onClick={() => setAlertvisible(true)}> My Button </Button>
+        <div>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <h1>Render Count: {renderCount.current}</h1>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ muz-react-app/src/components/GitHubUser.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GitHubUser = ({ nam, tit }: any) => {
+  return (
+    <div>
+      <h1>{nam}</h1>
+      <h1>{tit}</h1>
+    </div>
+  );
+};
+
+export default GitHubUser;

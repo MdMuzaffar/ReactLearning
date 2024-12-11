c65bd4217bb82c995df2b0dde72124c8b52a3cbd
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2024-12-11 23:57:20 +0530

    Creating New application in React

--- muz-react-app/src/App.tsx
+++ muz-react-app/src/App.tsx
@@ -1,71 +1,74 @@
 import { useState, useEffect, useRef } from "react";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/alert";
-import GitHubUser from "./components/GitHubUser";
-// import React, { useState, useEffect, useRef } from 'react';
-
-// function GitHubUser({ title, name }: any) {
-//   return (
-//     <div>
-//       <h1>{name}</h1>
-//       <h1>{title}</h1>
-//     </div>
-//   );
-// }
+// import Button from "./components/Button";
+// import ListGroup from "./components/ListGroup";
+// import Alert from "./components/alert";
+// import GitHubUser from "./components/GitHubUser";
+// import React, { useState, useEffect, useRef } from "react";
 
 function App() {
-  let items = ["New York", "Tokiya", "London", "Paris"];
+  //   return (
+  //     <div>
+  //       <h1>{name}</h1>
+  //       <h1>{title}</h1>
+  //     </div>
+  //   );
+  // }
 
-  const [alertVisible, setAlertvisible] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const renderCount = useRef(0);
+  // function App() {
+  //   let items = ["New York", "Tokiya", "London", "Paris"];
+
+  //   const [alertVisible, setAlertvisible] = useState(false);
+  //   const [inputValue, setInputValue] = useState("");
+  //   const renderCount = useRef(0);
 
   // useEffect(() => {
   //   renderCount.current += 1;
   // });
 
-  const handleSelectItem = (item: String) => {
-    console.log(item);
-  };
+  // const handleSelectItem = (item: String) => {
+  //   console.log(item);
+  // };
 
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    fetch(`https://api.github.com/users/moonhighway`)
-      .then((response) => response.json())
-      .then(setData);
-  }, []);
-  if (data)
-    return (
-      <div>
-        <div>
-          <GitHubUser
-            name={data?.name}
-            location={data?.location}
-            avatar={data?.avatar_url}
-            createdDate={data?.created_at}
-            updatedDate={data?.updated_at}
-            bio={data?.bio}
-          />
-        </div>
+  // const [data, setData] = useState(null);
+  // useEffect(() => {
+  //   fetch(`https://api.github.com/users/moonhighway`)
+  //     .then((response) => response.json())
+  //     .then(setData);
+  // }, []);
+  // if (data)
+  return (
+    // <div>
+    //   <div>
+    //     <GitHubUser
+    //       name={data?.name}
+    //       location={data?.location}
+    //       avatar={data?.avatar_url}
+    //       createdDate={data?.created_at}
+    //       updatedDate={data?.updated_at}
+    //       bio={data?.bio}
+    //     />
+    //   </div>
 
-        <ListGroup
-          items={items}
-          heading="Cities"
-          onSelectItem={handleSelectItem}
-        />
-        {alertVisible && <Alert onClose={() => setAlertvisible(false)}></Alert>}
-        <Button onClick={() => setAlertvisible(true)}> My Button </Button>
-        <div>
-          <input
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <h1>Render Count: {renderCount.current}</h1>
-        </div>
-      </div>
-    );
+    //   <ListGroup
+    //     items={items}
+    //     heading="Cities"
+    //     onSelectItem={handleSelectItem}
+    //   />
+    //   {alertVisible && <Alert onClose={() => setAlertvisible(false)}></Alert>}
+    //   <Button onClick={() => setAlertvisible(true)}> My Button </Button>
+    //   <div>
+    //     <input
+    //       type="text"
+    //       value={inputValue}
+    //       onChange={(e) => setInputValue(e.target.value)}
+    //     />
+    //     <h1>Render Count: {renderCount.current}</h1>
+    //   </div>
+    // </div>
+    <>
+      <h1>Hello React Testing</h1>
+    </>
+  );
 }
 
 export default App;

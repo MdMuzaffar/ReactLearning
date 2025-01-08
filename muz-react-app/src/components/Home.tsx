489aab055280669c28ef081918512dac9375f052
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2025-01-08 23:58:01 +0530

    Using Redux to add items in cart

--- muz-react-app/src/components/Home.tsx
+++ muz-react-app/src/components/Home.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import "../App.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Component</h1>
+    <div className="container">
+      <div className="cart-section">
+        <h1>Home Component</h1>
+        <div className="cart-image">
+          <img
+            src="https://static.vecteezy.com/system/resources/previews/000/644/217/original/shopping-cart-icon-vector.jpg"
+            alt="cart"
+          />
+        </div>
+      </div>
       <div className="main-cart">
-        <div>
+        <div className="main-image">
           <img
             src="https://images.pexels.com/photos/1092644/pexels-photo-1092644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt="Phone"
@@ -15,9 +24,9 @@ const Home = () => {
           <span>I-Phone</span>
           <span>Price : $ 1000.00</span>
         </div>
-      </div>
-      <div>
-        <button>Add to cart</button>
+        <div>
+          <button className="btn btn-primary">Add to cart</button>
+        </div>
       </div>
     </div>
   );
